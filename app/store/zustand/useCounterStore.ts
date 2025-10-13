0fc0c5d1db199045f6a00acb35021369063037b9
SanjayAlbiorix <sanjayp.albiorix@gmail.com>
2025-10-13 15:46:08 +0530

    simple fix

--- app/store/zustand/useCounterStore.ts
+++ app/store/zustand/useCounterStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { delay } from "../utils/delay";
+import { delay } from "../../utils/delay";
 
 interface CounterState {
   count: number;
